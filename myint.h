7c252e1db2ac5dbb371dd7938dc65c169c7dfd6c
Axel <axreich@gmail.com>
2024-10-30 02:41:50 -0400

    Modified the h file to my requirements, should be finished the .h file

--- myint.h
+++ myint.h
@@ -22,22 +22,46 @@ class MyInt
     friend bool operator!= (const MyInt& x, const MyInt& y);
 
     // declare overloads for input and output 
-    friend ostream& operator << (ostream& os, const MyInt& x);
-    friend istream& operator >> (istream& is, const MyInt& x);
+    friend ostream& operator << (ostream& os, const MyInt& x);      // Print the number in regular formatting 
+    friend istream& operator >> (istream& is, const MyInt& x);      // Ignore leading spaces, until there is a num. 
 
 
 public:
-   MyInt(int n = 0);		// first constructor
+    //Constructors 
 
-    //TODO: Add the other conversor constructors 
-    // be sure to add in the second constructor, and the user-defined 
-    //  versions of destructor, copy constructor, and assignment operator
+    MyInt(int n = 0);		    // If negative parameter, set to 0. Else, set value to the parameter 
+    MyInt(const char* i);       // C-string that is a combination of chars with a '\n' at the end. If Empty, or have chars, or does not have any nums 
+                                                                                                // integer = 0
+    ~MyInt();                   // When myInt is deallocated, should deallocate any data 
 
+    // Copy constructor and assignment operator for deep copy
+    MyInt(const MyInt& i);
+    MyInt operator=(const MyInt& i);
 
+    // getters
+    int GetSize()  const;
+    int GetArray() const;
+
+    // setters 
+    int SetSize();
+
+    //I Thint that SetArray is not necessary ????
+    int SetArray();
+    int Grow();
+
+    // Other operator overloads  ++int; int++
+        // Pre, returns the value incremented 
+    MyInt operator++();
+        // Post, returns a copy before incrementing the value
+    MyInt operator++(int x);
 
 private:
+    int size;
+    int *integer[];               // integer = [1,2,3,4,5]      When printed should be: 12345
 
-   // Add member data to count the size of the array and the array 
-   // member data (suggested:  use a dynamic array to store the digits)
+    void Resize(int NewSize);     // I should never put it more than 5 spots more than there are 
+                                    // Normally use it when the istream is called and the user has to type the int.  
 
 };
+
+// Is all this data enough????
